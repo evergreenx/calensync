ffeee7bd61a4734e51caba96c5b16cf236c41018
ido evergeen <dicksonevergreenido@gmail.com>
2023-05-17 02:00:00 +0100

    add avatar

---
+++ components/ui/Index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import HeaderClient from "./header";
+
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import { cookies } from "next/headers";
+// import { Database } from "@/lib/db.types";
+
+const links = ["Find Talent", "For Designers", "Learn Design", "Go Pro"];
+
+export const dynamic = "force-dynamic";
+
+async function Index() {
+  const supabase = createServerComponentClient<any>({ cookies });
+
+  const { data } = await supabase.auth.getSession();
+
+  console.log(data)
+
+
+  return (
+    <div>
+      {" "}
+      {/* <HeaderClient data={data} /> */}
+
+      <HeaderClient data={data}/>
+    </div>
+  );
+}
+
+export default Index;
