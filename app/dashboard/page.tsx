fa8b45344ba4a4851f17fc892091152e2d0f1b55
ido evergeen <dicksonevergreenido@gmail.com>
2023-11-16 17:00:00 +0100

    testing feature

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,33 +2,29 @@
 
 import * as React from "react";
 
-
 import {
+  Session,
   createClientComponentClient,
   createServerComponentClient,
 } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/lib/database.types";
 import { cookies } from "next/headers";
 import { UpdateProfileModal } from "@/components/ui/dashboard/updateProfileModal";
+import { Profiles } from "@/global";
 
 export default async function Page() {
   const supabase = createServerComponentClient<Database>({ cookies });
-  const { data } = await supabase.from("profiles").select();
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  const handleNewUser = (profiles: any) => {
-    if (!profiles || !Array.isArray(profiles)) {
-      // Handle the case where profiles is not available or not an array
-      return [];
-    }
-
-    return profiles.some((profile) => profile && profile.is_new_user === true);
-  };
-
-  console.log(data)
+  const user: Session | any = session;
+  const { data } = await supabase
+    .from("profiles")
+    .select()
+    .eq("id", user?.user.id)
+    .single();
 
   return (
     <section className="my-20">
@@ -36,10 +32,10 @@ export default async function Page() {
       error eum quae aperiam? Quidem corporis, quasi similique amet aut
       inventore debitis, dolore tempore dignissimos suscipit facilis? Debitis,
       earum possimus.
-      {handleNewUser(data) && (
+      {data?.is_new_user && (
         <UpdateProfileModal
-          session={session}
-          openState={handleNewUser(data)}
+          session={data}
+          openState={data.is_new_user}
         ></UpdateProfileModal>
       )}
     </section>
